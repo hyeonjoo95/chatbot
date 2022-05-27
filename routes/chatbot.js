1a410c1af0d555d1e2ce7e9b9b7dd4622ffaacc4
stzhyeonjoo <hyeonjoo.yu@sundaytoz.com>
2022-05-27 22:12:58 +0900

    주석 오타 수정

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -9,7 +9,7 @@ mysqlConObj.open(db);
 /**
  * @path {GET} http://localhost:3000/chatbot/skill/start
  * @description 요청 데이터 값이 없고 반환 값이 있는 GET Method
-**/
+*/
 router.get('/skill/start', (req, res) => {
   var sql = "SELECT contents FROM welcome_talk order by rand() limit 1;" + 
             "SELECT id FROM card_data;"
@@ -34,7 +34,7 @@ router.get('/skill/start', (req, res) => {
 /**
  * @path {GET} http://localhost:3000/chatbot/card/pick/:card_id
  * @description 요청 데이터 값이 있고 반환 값이 있는 GET Method
-**/
+*/
 router.get('/card/pick/:card_id', (req, res) => {
   var params = req.params;
   
